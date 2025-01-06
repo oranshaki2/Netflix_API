ebd5ce0a6c4e789062f2956be39f159098b6eeda
Oran Shaki <oranshaki2@gmail.com>
2025-01-06 10:51:43 +0200

    Refactor token handling: update user authentication logic, improve variable naming, and enhance error responses for better clarity.

--- services/token.js
+++ services/token.js
@@ -1,25 +1,13 @@
-const Movie = require('../models/movie');
+const User = require('../models/user');
 
 // Check if the user is registered by given userName and password
-const checkIfUserRegistered = async (userName, password, res) => {
-    // const bodyJson = req.body;
-    // const { userName, password } = bodyJson;
-    // Fetch the user by userName and password 
-    const user = await tokenService.getUserByUserNameAndPassword(userName, password);
-    
-         // If the userName and password belong to registered user
-         if (user) {
-            return res.json({ id: user.id });
-        // If the userName and password do not belong to a registered user
-         } else {
-                return res.status(404).json({ errors: ['User name or password are not associated with registered user'] });
-         }
-
+const checkIfUserRegistered = async (username, password, res) => {
+    const user = await getUserByUserNameAndPassword(username, password);
 }; // It will change in the next exercise
 
 // Get a user by userName and password
-const getUserByUserNameAndPassword = async (userName, password) => {
-    return await users.findOne({ userName, password }); // Find user by userName and password
+const getUserByUserNameAndPassword = async (username, password) => {
+    return await User.findOne({ username, password }); // Find user by userName and password
 };
 
-module.exports = { checkIfUserRegistered, getUserByUserNameAndPassword };
+module.exports = { checkIfUserRegistered, getUserByUserNameAndPassword};
